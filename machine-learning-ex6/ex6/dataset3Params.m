e700c628ec965cd843721216e0029e4731553b8a
Craig <972350+craigls@users.noreply.github.com>
2021-02-15 21:24:09 -0400

    Week 7 completed

---
+++ machine-learning-ex6/ex6/dataset3Params.m
@@ -0,0 +1,59 @@
+function [C, sigma] = dataset3Params(X, y, Xval, yval)
+%DATASET3PARAMS returns your choice of C and sigma for Part 3 of the exercise
+%where you select the optimal (C, sigma) learning parameters to use for SVM
+%with RBF kernel
+%   [C, sigma] = DATASET3PARAMS(X, y, Xval, yval) returns your choice of C and 
+%   sigma. You should complete this function to return the optimal C and 
+%   sigma based on a cross-validation set.
+%
+
+% You need to return the following variables correctly.
+C = 1;
+sigma = 0.3;
+
+% ====================== YOUR CODE HERE ======================
+% Instructions: Fill in this function to return the optimal C and sigma
+%               learning parameters found using the cross validation set.
+%               You can use svmPredict to predict the labels on the cross
+%               validation set. For example, 
+%                   predictions = svmPredict(model, Xval);
+%               will return the predictions on the cross validation set.
+%
+%  Note: You can compute the prediction error using 
+%        mean(double(predictions ~= yval))
+%
+
+% Recommended values to try for C and sigma
+steps = [0.01 0.03 0.1 0.3 1 3 10 30];
+
+% Matrix for storing each iteration of C, sigma, error 
+results = zeros(length(steps) * length(steps), 3)
+
+row = 1;
+
+for i=1:length(steps)
+    for j=1:length(steps)
+        tmpC = steps(:, i);
+        tmpSigma = steps(:, j);
+
+        % Train the model then calculate accuracy
+        model = svmTrain(X, y, tmpC, @(x1, x2) gaussianKernel(x1, x2, tmpSigma));
+        pred = svmPredict(model, Xval);
+        err = mean(double(pred ~= yval))
+       
+        % Store results for later
+        results(row, :) = [tmpC, tmpSigma, err];
+        row = row + 1;
+
+end
+
+% Find row containing minimum error
+[_, row] = min(results(:, 3))
+
+% Set C and sigma to row containing min error
+C = results(row, 1);
+sigma = results(row, 2);
+
+% =========================================================================
+
+end
